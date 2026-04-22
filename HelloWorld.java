802a0a4e45639bb0c6d7a799798e0ddd4a0c6184
Ganesh Vasant Gaikwad <163426606+gvgaikwad@users.noreply.github.com>
2026-04-22 15:31:54 +0530

    Update greeting message in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Ganesh Gaikwad are you intrasted to become a Cloud Architect or AI Automation Engineer or DevOps & DevSecOps Engineer !");
     }
 }
